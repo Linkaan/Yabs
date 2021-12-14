d58a6cafb71ae89aeb613f634ca89f58e326fe98
Evelina K <89128496+ntijoh-evelina-karlsson@users.noreply.github.com>
2021-12-14 11:23:02 +0100

    Update LoanFormComponent.vue

--- frontend/src/components/LoanFormComponent.vue
+++ frontend/src/components/LoanFormComponent.vue
@@ -69,7 +69,7 @@
           <template v-slot:activator="{ on, attrs }">
             <v-text-field
               v-model="form.expiration_date"
-              label="Återlämnings datum"
+              label="Datum för återlämning"
               outlined
               v-bind="attrs"
               data-jest="expirationDate"
